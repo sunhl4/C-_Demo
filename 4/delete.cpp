943b750432f3becb211836eedb9eb9f36065fdf1
sunhl4 <15051962326@163.com>
2023-08-10 09:01:38 +0800

    new

---
+++ 4/delete.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+char * getname(void); //function prototype
+int main()
+{
+    char * name;
+    name = getname();//assign address of string to name 
+    cout << name << " at " << (int *) name << endl;
+    delete [] name;
+    name = getname();
+    cout << name << " at " << (int *) name << endl;
+    delete [] name;   
+
+    return 0;
+}
+
+char * getname()
+{
+    char temp[80];
+    cout << "Enter last name: ";
+    cin >> temp;
+    char * pn = new char[strlen(temp) + 1];
+    strcpy(pn, temp);
+    return pn;
+
+}
