09fccd2e745c7779ccf0100667d60f724df89090
sunhl4 <15051962326@163.com>
2023-09-04 21:51:59 +0800

    new

---
+++ 5/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+	// bool flag = true;
+	// cout << flag << endl; // 1
+	// flag = false;
+	// cout << flag << endl; // 0
+	// cout << "size of bool = " << sizeof(flag) << endl; //1
+	// system("pause");
+    char ch = 0;
+    cin >> ch;
+    
+	return 0;
+}
