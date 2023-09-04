19b7a6a4c1d38b2e257cd2514dbc92e868293d57
sunhl4 <15051962326@163.com>
2023-09-04 23:23:46 +0800

    new

--- 5/test.cpp
+++ 5/test.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() 
+void swap(int num1, int num2)
 {
-	// bool flag = true;
-	// cout << flag << endl; // 1
-	// flag = false;
-	// cout << flag << endl; // 0
-	// cout << "size of bool = " << sizeof(flag) << endl; //1
-	// system("pause");
-    char ch = 0;
-    cin >> ch;
-    
+	cout << "交换前：" << endl;
+	cout << "num1 = " << num1 << endl;
+	cout << "num2 = " << num2 << endl;
+
+	int temp = num1;
+	num1 = num2;
+	num2 = temp;
+
+	cout << "交换后：" << endl;
+	cout << "num1 = " << num1 << endl;
+	cout << "num2 = " << num2 << endl;
+
+	//return ; 当函数声明时候，不需要返回值，可以不写return
+}
+
+int main() {
+
+	int a = 10;
+	int b = 20;
+	swap(a, b);
+	cout << "mian中的 a = " << a << endl;
+	cout << "mian中的 b = " << b << endl;
+	system("pause");
 	return 0;
 }
