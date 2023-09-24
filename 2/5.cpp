0ffc008f8e71025d0f114bc38904d0be577bf3bf
sunhl4 <15051962326@163.com>
2023-09-24 22:24:32 +0800

    new

--- 2/5.cpp
+++ 2/5.cpp
@@ -8,6 +8,7 @@ int main(void)
     cout << "Enter the floor area, in square feet, of your home: ";
     cin >> area;
     double side;
+    
     side = sqrt(area);
     cout << "That's the equivalent of a square " << side << " feet to the side." << endl;
     cout << "How fascinating!" << endl; 
