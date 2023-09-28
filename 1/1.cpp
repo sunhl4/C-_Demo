f0be2ad38e0280bbd917ed34268bae743bbc2516
sunhl4 <15051962326@163.com>
2023-09-28 23:25:46 +0800

    new

--- 1/1.cpp
+++ 1/1.cpp
@@ -5,14 +5,36 @@ int main()
 // //    std::cout << "/*";
 //     // std::cout << "*/";
 //     std::cout << /* "*/" */;
-    int count = 1;
-    int sum = 0;
-    while (count <= 10)
+    // int count = 1;
+    // int sum = 0;
+    // while (count <= 10)
+    // {
+    //     sum += count;
+    //     ++ count;
+    // }
+    // std::cout << sum << std::endl;
+    int small, big;
+    int num;
+    std::cout << "Please enter two number (int)\n";
+    std::cin >> small >> big;
+    if (small <= big)
     {
-        sum += count;
-        ++ count;
+        num = small;
+        while(num < big)
+        {
+            ++ num;
+            std::cout << num << std::endl;
+        }
     }
-    std::cout << sum << std::endl;
+    else
+    {
+        num = big;
+        while(num < small)
+        {
+            ++ num ;
+            std::cout << num << std::endl;
+        }
+    }
 
     return 0;
 }
