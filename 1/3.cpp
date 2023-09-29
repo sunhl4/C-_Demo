eea5a215e03b506bfb1118a2c2ac1a1085b78df7
sunhl4 <15051962326@163.com>
2023-09-29 08:55:08 +0800

    new

---
+++ 1/3.cpp
@@ -0,0 +1,23 @@
+//count the frequency of input number
+#include <iostream>
+ int main()
+ {
+    int val, value, count=1;
+    if(std::cin >> val) //judge the input
+    {
+        while(std::cin >> value) 
+        {
+            if(value == val)
+            {
+                ++count;
+            }
+            else
+            {
+                std::cout << val << " occurs" << count << " times.\n";
+                val = value; //exchange the  compared number 
+            }
+        }
+        std::cout << val << " occurs" << count << " times.\n";
+    }
+    return 0;
+ }
